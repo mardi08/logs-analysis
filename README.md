3f6de319c469c7223509c4f0f1f9c629209c147c
vagrant <vagrant@vagrant.vm>
2018-08-03 20:25:26 +0000

    added more context to readme, adjusted errors.py to single query

--- README.md
+++ README.md
@@ -5,11 +5,29 @@ The program runs the following analysis based on data in ```news``` database:
 - ```popular_articles.py``` : Produced report of 3 most popular authors.
 - ```errors.py```: Produced report of days which more than 1 % of requests lead to errors.
 
-### Dependencies
+## Code Design
+
+**popular_articles**
+
+The program takes necessary information from the ```log``` table which then is joined with ```articles``` table. The joining process involved matching the slugs from each table as its primary key. The process is coded within a single query and python script is written to present output information in organized way
+
+**popular_authors**
+
+Sql script is also done in a single query. The *popular_articles.py* query is used with a condition of matching authors id from log table and popular articles table. Again, python script is written to present output information in organized way.
+
+Both ```popular_articles.py``` and ```popular_authors.py``` are set to show the top 3. However, adjustmenst can be made by changing the global variable ```TOP_COUNT``` that exists in both executable python files.
+
+**errors**
+
+sql script is done in a single query. The query is structured by extracting new tables individually and group by dates. One table takes rows based on success responses (with status:```200 OK```) and the other takes rows based on failed responses. After joining both tables, the query does the calculation and filter out any error rates below 1%.
+
+
+### Requirements
 ___
 You will need the following dependencies installed on your system to run the programs:
 - Python 2.7
 - PostgresSQL
+- psycopg2
 
 
 ### How to run the program
--- newsdata/errors.py
+++ newsdata/errors.py
@@ -15,40 +15,40 @@ cur = db.cursor()
 '''
 Constructing sql queries
 '''
-# query to count rows with error status from log table
-errors_query = "(" \
-               + "SELECT time::date, count(time::date) as errors \
-               from log where log.status != '200 OK' group by \
-               time::date order by time::date desc" \
-               + ")"
-# query to count rows with success status from log table
-success_query = "(" \
-                + "SELECT time::date, count(time::date) as success \
-                from log where log.status = '200 OK' group by \
-                time::date order by time::date desc" \
-                + ")"
-# joining errors and success new tables
-errors_success_query = "(" + \
-                       "SELECT log_success.time::date, log_success.success,\
-                       log_errors.errors from " \
-                       + success_query \
-                       + " as log_success join " \
-                       + errors_query \
-                       + " as log_errors on log_success.time::date \
-                       = log_errors.time::date order by log_success.time::date desc" \
-                       + ")"
-# calculate the error rate in percent
-calc_query = "(" \
-             + "SELECT time::date, (round((100 * ((1.0 * errors) \
-             / ((1.0 * success) + (1.0 * errors)) )),2))\
-              as error_rates from " \
-             + errors_success_query \
-             + "as status_summary" \
-             + ")"
-# filtering out unnecessary information
-final_query = "SELECT * from " \
-              + calc_query \
-              + " as error_table where error_table.error_rates > 1.0;"
+
+final_query = """
+SELECT *
+FROM
+  (SELECT TIME::date,
+          (
+              round(
+                  (100 * (
+                      (1.0 * errors)/((1.0 * success) + (1.0 * errors)))),2
+                    )
+            )
+          AS error_rates
+   FROM
+     (SELECT log_success.time::date,
+             log_success.success,
+             log_errors.errors
+      FROM
+        (SELECT TIME::date,
+                count(TIME::date) AS success
+         FROM log
+         WHERE log.status = '200 OK'
+         GROUP BY TIME::date
+         ORDER BY TIME::date DESC) AS log_success
+      JOIN
+        (SELECT TIME::date,
+                count(TIME::date) AS errors
+         FROM log
+         WHERE log.status != '200 OK'
+         GROUP BY TIME::date
+         ORDER BY TIME::date DESC)
+         AS log_errors ON log_success.time::date = log_errors.time::date
+      ORDER BY log_success.time::date DESC) AS status_summary) AS error_table
+WHERE error_table.error_rates > 1.0;
+"""
 
 # Getting all queries
 cur.execute(final_query)
